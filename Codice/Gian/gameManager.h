95a24cce0ba884a8ffc20dcd77014968399357f2
Mandr96 <gianmarcolembo@gmail.com>
2023-03-31 21:33:46 +0200

    gamemanager files added

---
+++ Codice/Gian/gameManager.h
@@ -0,0 +1,22 @@
+#ifndef GAMEMANAGER_H
+#define GAMEMANAGER_H
+#include <netinet/in.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <pthread.h>
+#include <stdbool.h>
+#include "strutture.h"
+
+#define PORT 9876
+#define MAX_CLIENTS 1
+#define BUFFDIM 1024
+#define NUMBER_OF_SUGGESTED_WORD 4
+
+void start_game(stanza*);
+void start_round(stanza*, int);
+void prepareWords(char dest[BUFFDIM], char *parole[NUMBER_OF_SUGGESTED_WORD][2]);
+
+#endif
