5ee30b88bc373206064514918a4a2a416b5b4d6d
CristianCarrella <72307833+CristianCarrella@users.noreply.github.com>
2023-04-04 21:32:31 +0200

    Commit

--- Codice/__Final__/db-setup.sql
+++ Codice/__Final__/db-setup.sql
@@ -3,20 +3,8 @@ CREATE TABLE utente(
 	passw varchar(16) NOT NULL,
 	email varchar(64) NOT NULL,
 	partiteVinte integer DEFAULT 0,
-	idStanza integer DEFAULT NULL
+	imgId integer DEFAULT 0
 );
-
-CREATE TABLE stanza(
-	idStanza serial NOT NULL PRIMARY KEY,
-	nomeStanza varchar(16) NOT NULL,
-	numeroMax integer NOT NULL,
-	privato bool DEFAULT 'false',
-	keyword varchar(16) DEFAULT '',
-	adminUsername varchar(32) NOT NULL
-);
-
-ALTER TABLE stanza ADD CONSTRAINT FK_Sadmin FOREIGN KEY (adminUsername) REFERENCES utente(username) ON DELETE CASCADE;
-ALTER TABLE utente ADD CONSTRAINT FK_Ustanza FOREIGN KEY (idStanza) REFERENCES stanza(idStanza) ON DELETE SET NULL;
 
 CREATE TABLE parola(
 	word varchar(16) NOT NULL PRIMARY KEY
