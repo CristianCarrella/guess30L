4772a3f48913685bf91bc9a4c48dca148aaf554f
Mandr96 <98476394+Mandr96@users.noreply.github.com>
2023-05-01 22:05:08 +0200

    Update gameManager.h

--- Codice/__Final__/header/gameManager.h
+++ Codice/__Final__/header/gameManager.h
@@ -8,6 +8,8 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <stdbool.h>
+#include <json-c/json.h>
+#include <time.h>
 #include "strutture.h"
 
 #define PORT 9876
@@ -18,5 +20,7 @@
 void start_game(stanza*);
 void start_round(stanza*, int);
 void prepareWords(char dest[BUFFDIM], char *parole[NUMBER_OF_SUGGESTED_WORD][2]);
+void sendBroadcast(stanza* room, int idHost, char* msg);
+struct json_object *generateHint(char *parola, int *hints);
 
 #endif
