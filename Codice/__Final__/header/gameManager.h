972156848124f2c37602617ac1856af9933602c5
Mandr96 <98476394+Mandr96@users.noreply.github.com>
2023-05-15 19:28:09 +0200

    Update gameManager.h

--- Codice/__Final__/header/gameManager.h
+++ Codice/__Final__/header/gameManager.h
@@ -11,16 +11,26 @@
 #include <json-c/json.h>
 #include <time.h>
 #include "strutture.h"
+#include <sys/time.h>
 
 #define PORT 9876
 #define MAX_CLIENTS 1
 #define BUFFDIM 1024
 #define NUMBER_OF_SUGGESTED_WORD 4
 
-void start_game(stanza*);
-void start_round(stanza*, int);
-void prepareWords(char dest[BUFFDIM], char *parole[NUMBER_OF_SUGGESTED_WORD][2]);
-void sendBroadcast(stanza* room, int idHost, char* msg);
+struct clientData
+{
+    int socket;
+    stanza *room;
+}
+clientData;
+
+char *start_room(stanza*, int);
+// Ritorna l'index del vincitore all'interno dell'array players
+int start_round(stanza*, int);
 struct json_object *generateHint(char *parola, int *hints);
+void sendBroadcast(stanza*, int, char*);
+void prepareWords(char dest[BUFFDIM], char *parole[NUMBER_OF_SUGGESTED_WORD][2]);
+int hintsIsFull(int *hints, int len);
 
 #endif
