1172429a47829f84a1651f19b586f48113bf0ea1
Fedegar <federicogargiulo1999@gmail.com>
2023-05-25 19:17:53 +0200

    Ops

--- Codice/__Final__/src/serverTest.c
+++ Codice/__Final__/src/serverTest.c
@@ -87,7 +87,7 @@ void *handle2_client(void *par_) {
             char* base64image_encoded = getAvatarByEmail(conn, email,  pthread_self());
 
             json_object_object_add(json, "email", json_object_new_string(email));
-            json_object_object_add(json, "avatarBase64", json_object_new_string("base64image_encoded")); //al client arriverà con dei backslash in più base64image_encoded
+            json_object_object_add(json, "avatarBase64", json_object_new_string(base64image_encoded)); //al client arriverà con dei backslash in più base64image_encoded
             sendResponse(json, socket);
         }
         else if(strcmp(operation, "setAvatar") == 0){
